5226bdd9966cc4ba72f15e6daa59601b043313b0
carosales820 <cteig@engr-131-25.eastern.ewu.edu>
2015-02-18 13:50:33 -0800

    section2
    fix var and display

--- 2/js/scripts.js
+++ 2/js/scripts.js
@@ -1,13 +1,17 @@
 // Close Button
-
+$('.message .close').on('click', function() {
+  $(this).closest('.message').fadeOut(500);
+});
 
 // Previous Number of Messages
 var prevNumMsg = 35;
 // New Number of Messages
 var newNumMsg = 123;
 // Create variable, `totalMsg` to store Total Messages
+var totalMsg = totalMsg;
 // Add (sum) of Previous and New Messages
-var totalMsg;
+var totalMsg = prevNumMsg + newNumMsg;
 
 // Output to red Messages badge
 // REF: http://api.jquery.com/text/
+$('.floating.ui.red.label.msg).text(totalMsg);
