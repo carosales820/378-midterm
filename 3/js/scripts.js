916b42cc13912b5237f7670322849774930a27be
carosales820 <cteig@engr-131-25.eastern.ewu.edu>
2015-02-18 14:11:25 -0800

    Section3
    complete

--- 3/js/scripts.js
+++ 3/js/scripts.js
@@ -1,18 +1,23 @@
 // Load (run) modal on page load event
-// Use `show` option/parameter
 
+// Use `show` option/parameter
+$('.ui.modal')
+  .modal('show')
+;
 // Random Number Generator
 // returns 0 to 'limit' or 3 numbers
 var limit = 3;
 var numRand = Math.floor(Math.random()*limit);
 
 // Store 3 separate variables into a single array
-// var heading0 = 'Everything is Awesome!';
-// var heading1 = 'Be the Good.';
-// var heading2 = 'Dont Panic & Carry On.';
+//var heading0 = 'Everything is Awesome!';
+//var heading1 = 'Be the Good.';
+//var heading2 = 'Dont Panic & Carry On.';
 
-
+var heading = ['Everythign is Awesome!', 'Be the Good.', 'Dont Panic & Carry on.']
 
 // Randomly select one-of-3 Headings on Page Load
+
 // Output to Heading
 // REF: http://api.jquery.com/text/
+$('.header').text (heading[numRand]);
